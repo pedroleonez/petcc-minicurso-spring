7da865324a0465a7b88b49071092caaff0b0dbe7
Pedro Vitor da Silva Melo Leonez <147958375+pedroleonez@users.noreply.github.com>
2026-02-24 08:58:49 -0300

    feat: add dia2 initial project

---
+++ dia2/src/main/java/pedroleonez/dia2/Dia2Application.java
@@ -0,0 +1,13 @@
+package pedroleonez.dia2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dia2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dia2Application.class, args);
+	}
+
+}
---
+++ dia2/src/test/java/pedroleonez/dia2/Dia2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pedroleonez.dia2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dia2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
